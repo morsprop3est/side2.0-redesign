4393f9d4370d5be8e5fbd6d2b7142814403c9af7
polandkyrva <polandkyrva@gmail.com>
2024-07-03 11:58:43 +0300

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {styles} from 'Header.module.scss';
+
+function Header() {
+    return (
+        <div>
+            <h1 className={styles.h1}>Nigger</h1>
+        </div>
+    )
+}
+
+export default Header
